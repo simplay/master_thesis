71513f6dbf123a5ce2dbb243e95ee6f866a914a9
simplay <michael.single@students.unibe.ch>
2015-09-02 13:13:03 +0200

    Initial version of tracking
    still rounding

---
+++ tracking/flow_sanity_check.m
@@ -0,0 +1,51 @@
+function [ mask ] = flow_sanity_check( foreward_flow, backward_flow )
+%FLOW_SANITY_CHECK Summary of this function goes here
+%   Detailed explanation goes here
+
+    [m,n,~] = size(foreward_flow);
+
+    %Idx = repmat((1:m)', 1, n);
+    %Idy = repmat((1:n), m, 1);
+
+    fw_u_flow = foreward_flow(:,:,1);
+    fw_v_flow = foreward_flow(:,:,2);
+
+    bw_u_flow = backward_flow(:,:,1);
+    bw_v_flow = backward_flow(:,:,2);
+    
+    mask = zeros(m,n);
+    for x_t=1:m,
+        for y_t=1:n,
+            u_t = fw_u_flow(x_t,y_t);
+            v_t = fw_v_flow(x_t,y_t);
+            
+            idx = round(u_t+x_t);
+            idy = round(v_t+y_t);
+            
+            if idx > 0 && idx <= m && idy > 0 && idy <= n
+                %u_del = abs(bw_u_flow(idx,idy) + fw_u_flow(x_t, y_t));
+                %v_del = abs(bw_v_flow(idx,idy) + fw_v_flow(x_t, y_t));
+                %if u_del < 1 && v_del < 1
+                %    mask(x_t, y_t) = 1;
+                %end
+                
+                u_tilde = bw_u_flow(idx,idy);
+                u = fw_u_flow(x_t, y_t);
+                
+                v_tilde = bw_v_flow(idx,idy);
+                v = fw_v_flow(x_t, y_t);
+                
+                w = [u,v];
+                w_tilde = [u_tilde, v_tilde];
+                
+                if norm(w+w_tilde)^2 < 0.01*(norm(w)^2 + norm(w_tilde)^2)+0.5
+                    mask(x_t, y_t) = 1;
+                end
+                
+            end
+            
+        end
+    end
+    disp('foobar');
+end
+
--- tracking/main.m
+++ tracking/main.m
@@ -4,38 +4,89 @@ close all;
 
 addpath('../libs/flow-code-matlab');
 
-dummyImg = imread('../data/ldof/cars1_01.ppm');
+base_img_idx = 1;
+
+path = '../data/ldof/';
+file = 'cars1_';
+format = '.ppm';
+prefix = '0';
+img1_filepath = [path,file,prefix,num2str(base_img_idx),format];
+img2_filepath = [path,file,prefix,num2str(base_img_idx+1),format];
+
+foreward_flow_filepath = [path,'ForwardFlow00',num2str(base_img_idx),'.flo'];
+backward_flow_filepath = [path,'BackwardFlow00',num2str(base_img_idx),'.flo'];
+
+dummyImg = imread(img1_filepath);
 [m,n,~] = size(dummyImg);
 
-img1 = imread('../data/ldof/cars1_01.ppm');
+img1 = imread(img1_filepath);
 img1 =im2double(img1);
-img2 = imread('../data/ldof/cars1_02.ppm');
+img2 = imread(img2_filepath);
 img2 =im2double(img2);
 
 
 
 sigma = 1;
 
-[pixel_values, pixel_mask] = find_tracking_candidates(img1, sigma, 0.3);
-optical_flow = readFlowFile('../data/ldof/ForwardFlow001.flo');
+thresh = 0.3;
+
+[pixel_values, pixel_mask] = find_tracking_candidates(img1, sigma, thresh);
+optical_flow = readFlowFile('../data/ldof/ForwardFlow004.flo');
+backward_optical_flow = readFlowFile('../data/ldof/BackwardFlow004.flo');
 % [I,J,V] = find(pixel_mask > 0);
 
 u_flow = optical_flow(:,:,1);
 v_flow = optical_flow(:,:,2);
+
+bw_u_flow = backward_optical_flow(:,:,1);
+bw_v_flow = backward_optical_flow(:,:,2);
 
 Idx = repmat((1:m)', 1, n);
 Idy = repmat((1:n), m, 1);
 
 pixel_mask = im2double(pixel_mask);
 
+
+sanity_mask = flow_sanity_check(optical_flow, backward_optical_flow);
+
 % perform bilinear interpolation instead of adding and cropping
-tp1_x_candidates = (u_flow+Idx).*pixel_mask;
-tp1_y_candidates = (v_flow+Idy).*pixel_mask;
+tp1_x_candidates = (u_flow+Idx).*pixel_mask.*sanity_mask;
+tp1_y_candidates = (v_flow+Idy).*pixel_mask.*sanity_mask;
 
 tp1_x_candidates = round(tp1_x_candidates);
 tp1_y_candidates = round(tp1_y_candidates);
 
-[ix,jx,~] = find(tp1_x_candidates >= 1);
-[iy,jy,~] = find(tp1_y_candidates >= 1);
+
+
+%%
+other = zeros(m,n);
+pm = zeros(m,n);
+counter = 0;
+THRESH = 2; % pixel thresh
+for k=1:m,
+    for l=1:n,
+        idx2 = tp1_x_candidates(k,l);
+        idy2 = tp1_y_candidates(k,l);
+        if idx2 > 0 && idy2 > 0 && idx2<=m && idy2 <= n
+            %if idx2+k <= m && idy2+l <= n
+                %cond1 = u_flow(k,l) + bw_u_flow(idx2+k,idy2+l);
+                %cond2 = v_flow(k,l) + bw_v_flow(idx2+k,idy2+l);
+
+                %if abs(cond1) < THRESH && abs(cond2) < THRESH
+                    counter = counter + 1;
+                    other(idx2, idy2) = 1;
+                    pm(k, l) = 1;
+                %end
+                
+            %end
+        end
+    end
+end
+disp('foobarized')
+
+%%
+dummy = zeros(m,n);
+imshow(mat2img(pm,dummy,dummy)+mat2img(dummy,dummy,other));
+disp('red: features frame t AND blue: shift to same features in frame t + 1')
 
 disp('done')
