2b2aadb218c90820dd5b8dccb6e70655646521a0
simplay <michael.single@students.unibe.ch>
2015-09-17 17:15:15 +0200

    Allow to use arbitrary datasets

--- tracking/main.m
+++ tracking/main.m
@@ -1,45 +1,55 @@
 clc;
 clear all;
 close all;
-
 addpath('../libs/flow-code-matlab');
 
-step_size = 4;
+ %% 
+STEP_SIZE = 4; % tracking density
+BASE_FILE_PATH = '../data/ldof/cars1/'; % dataset that should be used
+IM_EXT = '.ppm'; % input img file extension
+DISPLAY = true; % show tracking points
 
-img1 = imread('../data/ldof/cars1/01.ppm');
-img2 = imread('../data/ldof/cars1/02.ppm');
-img1 =im2double(img1);
-img2 =im2double(img2);
-
+%% working example
 % fix naming of files: since image naming indices start counting by 1 and
 % flow field by zero, there is a potential confusion.
 % for the first image k, we have to assign flow k-1 
-foreward_flow = readFlowFile('../data/ldof/cars1/ForwardFlow000.flo');
-backward_flow = readFlowFile('../data/ldof/cars1/BackwardFlow000.flo');
+% img1 = imread('../data/ldof/cars1/01.ppm');
+% img2 = imread('../data/ldof/cars1/02.ppm');
+% foreward_flow = readFlowFile('../data/ldof/cars1/ForwardFlow000.flo');
+% backward_flow = readFlowFile('../data/ldof/cars1/BackwardFlow000.flo');
+for t=1:1,
+    im_t = strcat(BASE_FILE_PATH,'0',num2str(t), IM_EXT);
+    im_tp1 = strcat(BASE_FILE_PATH,'0',num2str(t+1), IM_EXT);
+    fw_flow_t = strcat(BASE_FILE_PATH, 'ForwardFlow','00',num2str(t-1),'.flo');
+    bw_flow_t = strcat(BASE_FILE_PATH, 'BackwardFlow','00',num2str(t-1),'.flo');
+    [ tracked_pixels, trackable_pixels, invalid_regions ] = process_frame_pair( im_t, fw_flow_t, bw_flow_t, STEP_SIZE );
+    
+    if DISPLAY
+        img1 = imread(im_t);
+        img2 = imread(im_tp1);
+        img1 =im2double(img1);
+        img2 =im2double(img2);
+        
+        %% trackable refers to points that can be tracked
+        figure('name', 'trackable points (blue) on 1st img');
+        imshow(img1);
+        hold on;
+        [cidx, cidy, ~] = find(trackable_pixels(:,:,1) == 1);
+        plot(cidy, cidx, '.b')
 
-% foreward_flow = readFlowFile('../data/teddy/fw001.flo');
-% backward_flow = readFlowFile('../data/teddy/bw001.flo');
+        %% tracked points refers to points in frame t+1 that a trackable point lands on. 
+        figure('name', 'tracked to points (red) on 2nd img');
+        imshow(img2);
+        hold on;
+        [tidx, tidy, ~] = find(tracked_pixels(:,:,1) == 1);
+        plot(tidy, tidx, '.r')
 
-[ tracked_pixels, trackable_pixels, invalid_regions ] = perform_tracking_step(img1, foreward_flow, backward_flow, step_size);
+        %% 
+        figure('name', 'blue: trackable points, red: tracked to points');
+        imshow(img2);
+        hold on;
+        plot(tidy, tidx, '.r')
+        plot(cidy, cidx, '.b')
+    end
+end
 
-
-%%
-figure('name', 'tracking points (blue) on 1st img');
-imshow(img1);
-hold on;
-[cidx, cidy, ~] = find(trackable_pixels(:,:,1) == 1);
-plot(cidy, cidx, '.b')
-
-%%
-figure('name', 'tracked to points (red) on 2nd img');
-imshow(img2);
-hold on;
-[tidx, tidy, ~] = find(tracked_pixels(:,:,1) == 1);
-plot(tidy, tidx, '.r')
-
-%%
-figure('name', 'blue: tracking points, red: tracked to points');
-imshow(img2);
-hold on;
-plot(tidy, tidx, '.r')
-plot(cidy, cidx, '.b')
