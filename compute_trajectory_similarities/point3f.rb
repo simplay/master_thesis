ea1254f77e2f2962db86b885f91faca98ec91ed6
simplay <silent.simplay@gmail.com>
2015-10-27 16:29:03 +0100

    Introduced point3f class
    support trajectory points with a depth. should finally increase the
    accuracy of the segmentation.

---
+++ compute_trajectory_similarities/point3f.rb
@@ -0,0 +1,50 @@
+class Point3f
+
+  attr_reader :x, :y, :z
+
+  def initialize(args)
+    @x = args[0]
+    @y = args[1]
+    @z = args[z]
+  end
+
+  # Build a 3d point from a trajectory point.
+  #
+  # @param p [Point] 2d point from extracted trajectory
+  # @param frame_idx [Integer] depth map associated to given frame index.
+  # @return [Point3f] 2d trajectory point with depth.
+  def self.build_from(p, frame_idx)
+    z = 1.0 ## lookup from depth map using the frame index
+    # a bilinear interpolation will be required
+    # depth_map = $dmm.map_of(frame_idx)
+    # z = depth_map.interpolated_value(p)
+    Point3f.new([p.x, p.y, z])
+  end
+
+  # @todo: do not hardcode 480 and 640
+  def out_of_range?
+    x > 480 or y > 640 or x < 0 or y < 0
+  end
+
+  def copy
+    Point3f.new([x,y,z])
+  end
+
+  def sub(other)
+    @x = @x - other.x
+    @y = @y - other.y
+    @z = @z - other.z
+    self
+  end
+
+  # length according to the l2 norm.
+  def length
+    Math.sqrt(length_squared)
+  end
+
+  # squared length according to the l2 norm.
+  def length_squared
+    x**2 + y**2 + z**2
+  end
+
+end
