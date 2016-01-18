6b97994d3cb182831ffc36a400bc3131ceb751e0
simplay <silent.simplay@gmail.com>
2016-01-18 17:21:10 +0100

    reporting status during depth extractions

--- extract_depth/main.m
+++ extract_depth/main.m
@@ -1,9 +1,10 @@
-START_IDX = 92;
-END_IDX = 105;
-DATASET = 'teacan';
+START_IDX = 1% 250;
+END_IDX = 104% 351;
+DATASET = 'wh1';
 path = ['../data/ldof/', DATASET, '/depth/'];
 
 for k=START_IDX:END_IDX
+    disp(strcat('Iteration: ',num2str(k-START_IDX+1)))
     fpath = strcat(path, num2str(k), '.png');
     lv = imread(fpath);
     fname = strcat('../output/depths/',DATASET,'_depth_',num2str(k),'.txt');
