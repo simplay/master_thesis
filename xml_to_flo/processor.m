47504737115df6c451f7ad733005857c0c34f099
simplay <silent.simplay@gmail.com>
2016-01-18 17:25:05 +0100

    Modularized dataset loading
    allow to load arbitrary datasets plus reporting which data has been
    loaded.

--- xml_to_flo/processor.m
+++ xml_to_flo/processor.m
@@ -1,12 +1,13 @@
 addpath('util');
 addpath('../libs/flow-code-matlab');
 
-dataset = 'foo/';
+dataset = 'chair3/';
 in_path = strcat('input/',dataset);
-out_path = 'output/foo/';
+out_path = strcat('output/',dataset);
 
 filenames = dir(strcat('input/', dataset, '*.xml'));
 for k=1:length(filenames)
+    disp(strcat('Iteration: ', num2str(k)))
     fname = filenames(k).name;
     generate_flow(in_path, fname, out_path );
 end
