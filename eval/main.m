372c5d170a92040dbb80667ab52cc2e42ba47c87
simplay <silent.simplay@gmail.com>
2016-01-27 15:51:15 +0100

    Implemented simplified statistical evaluation
    currently not differentiating between label types, only whether samples
    is correctly classified as a foreground or background pixel.

--- eval/main.m
+++ eval/main.m
@@ -1,7 +1,7 @@
 addpath('../libs/flow-code-matlab');
 
 %% load ground truth img
-DS = 'cars1';
+DS = 'car2';
 imgName = '01.pgm';
 DS_BASE_PATH = strcat('../data/ldof/',DS,'/gt/',imgName);
 gtImg = imread(DS_BASE_PATH);
@@ -46,7 +46,7 @@ for k=1:length(row_inds)
     end
 
 end
-figure
+figure('name', 'all samples')
 dsImgShow = dsImg - min(dsImg(:));
 dsImgShow = dsImgShow ./ max(dsImgShow(:));
 imshow(im2double(dsImgShow));
@@ -63,6 +63,7 @@ for k=1:length(labels)
     end
 end
 
+allSamples = dsImg;
 [a,b,c] = find(dsImg == lid);
 for k=1:length(a)
     dsImg(a(k),b(k)) = 0;
@@ -70,13 +71,43 @@ end
 
 dsImgShow = dsImg - min(dsImg(:));
 dsImgShow = dsImgShow ./ max(dsImgShow(:));
-figure('name', 'sparse clusters');
+figure('name', 'sparse forground (cluster) samples');
 imshow(im2double(dsImgShow));
 
+% do the statistics here
+forgroundSamples = dsImg;
+backgroundSamples = (allSamples > 0)-(forgroundSamples > 0);
 
-matchMask = (dsImg > 0) & (gtImg > 0);
-totalHits = sum(sum(dsImg > 0));
-matchedHits = sum(sum(matchMask > 0));
+figure('name', 'sparse background (cluster) samples');
+imshow(im2double(backgroundSamples));
 
-disp(['ratio machtes of total hits:', num2str(matchedHits/totalHits)])
+
+% samples classified as forground that actually also belong to the
+% forground.
+TP = forgroundSamples.*((forgroundSamples > 0) & (gtImg > 0));
+TP_Count = sum(sum(TP > 0));
+
+% samples classified to background that actually belong to forground
+FN = backgroundSamples .*(backgroundSamples > 0) & (gtImg > 0);
+FN_Count = sum(sum(FN > 0));
+
+% samples classified to forground that do belong to the background
+FP = (forgroundSamples > 0) - (TP > 0);
+FP_Count = sum(sum(FP > 0));
+
+% measure between correct classifications and additional (wrong) hits.
+% can be good, if there are samples missing, but no uncorrect assignments
+% were made (extreme case, only one assignment 
+% (i.e. lots of missing classifications) but no wrong classifications) 
+precission = TP_Count / (TP_Count + FP_Count);
+
+% measure indicating some relationship between incorrectly classified
+% background samples (that should belong to the forground) and TP.
+% get worse the more samples are missing.
+recall = TP_Count / (TP_Count + FN_Count);
+F1_score = 2*((precission*recall) / (precission+recall));
+
+disp(['precission:', num2str(precission)])
+disp(['recall:', num2str(recall)])
+disp(['F1 score:', num2str(F1_score)])
 
