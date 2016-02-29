960edd7572b079b456787a1935346af678ff9d16
simplay <silent.simplay@gmail.com>
2016-03-01 00:41:20 +0100

    Changed parameter setup

--- kernighan_lin/src/main/java/com/ma/Main.java
+++ kernighan_lin/src/main/java/com/ma/Main.java
@@ -13,10 +13,10 @@ public class Main {
 
         String dataset = "c14";
         String baseOutputPath = "../output/graph_part/";
-        int clusterCount = 3;
+        int clusterCount = 5;
         int dummyCount = 500;
         int max_iterations = 10;
-        int repetitionCount = 4;
+        int repetitionCount = 6;
 
         Path currentRelativePath = Paths.get("");
         String s = currentRelativePath.toAbsolutePath().toString();
