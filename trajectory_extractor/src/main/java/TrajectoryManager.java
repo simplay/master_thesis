d3ecd8a8fa35884458cfe7a142ddf5d439198741
simplay <silent.simplay@gmail.com>
2016-03-04 20:27:04 +0100

    Allow to select all trajectories starting at a certain frame
    plus wrote docs

--- trajectory_extractor/src/main/java/TrajectoryManager.java
+++ trajectory_extractor/src/main/java/TrajectoryManager.java
@@ -5,9 +5,15 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedList;
 
+/**
+ * Singleton containing and managing all existing trajectories.
+ */
 public class TrajectoryManager implements Iterable<Trajectory>{
 
+    // A hash containing all the trajectories, with the trajectory label as key.
     private HashMap<Integer, Trajectory> trajectories;
+
+    // Singleton state
     private static TrajectoryManager instance = null;
 
     public static TrajectoryManager getInstance() {
@@ -17,19 +23,37 @@ public class TrajectoryManager implements Iterable<Trajectory>{
         return instance;
     }
 
-    public int trajectoryCount() {
-        return trajectories.size();
-    }
     private TrajectoryManager() {
         trajectories = new HashMap<Integer, Trajectory>();
     }
 
+    /**
+     * Number of trajectories this Manager contains.
+     *
+     * @return
+     */
+    public int trajectoryCount() {
+        return trajectories.size();
+    }
+
+    /**
+     * Start a new trajectory with a given starting point at a given starting frame.
+     *
+     * @param p starting point the new trajectory is supposed to start
+     * @param startingFrame frame index where the new trajectory will start at.
+     */
     public void startNewTrajectoryAt(Point2f p, int startingFrame) {
         Trajectory tra = new Trajectory(startingFrame);
         tra.addPoint(p);
         trajectories.put(tra.getLabel(), tra);
     }
 
+    /**
+     * Append a new tracking point to target trajectory.
+     *
+     * @param trajectory_label trajectory identifier referencing the target trajectory.
+     * @param p point to be added to the target trajectory
+     */
     public void appendPointTo(int trajectory_label, Point2f p) {
         Trajectory tra = trajectories.get(trajectory_label);
         tra.addPoint(p);
@@ -55,6 +79,12 @@ public class TrajectoryManager implements Iterable<Trajectory>{
         return actives;
     }
 
+    /**
+     * Select all trajectories that have a given length value.
+     *
+     * @param length expected trajectory length value.
+     * @return Collection of trajectories all having a expected length value.
+     */
     public LinkedList<Trajectory> allTrajectoryWithLength(int length) {
         LinkedList<Trajectory> actives = new LinkedList<Trajectory>();
         for (Trajectory tra : trajectories.values()) {
@@ -65,12 +95,38 @@ public class TrajectoryManager implements Iterable<Trajectory>{
         return actives;
     }
 
+    /**
+     * Select all trajectories that start at a given frame.
+     *
+     * @param frame_idx index of frame the trajectories should start.
+     * @return a collection of trajectories all starting at a given frame.
+     */
+    public LinkedList<Trajectory> allTrajectoriesStartingAt(int frame_idx) {
+        LinkedList<Trajectory> actives = new LinkedList<Trajectory>();
+        for (Trajectory tra : trajectories.values()) {
+            if (tra.getStartFrame() == frame_idx) {
+                actives.add(tra);
+            }
+        }
+        return actives;
+    }
+
+    /**
+     * Filter all trajectories from the internal list of trajectories,
+     * that have length zero, i.e. that consists of just one point.
+     */
     public void filterOnePointedTrajectories() {
         for (Trajectory traj : allTrajectoryWithLength(0)) {
             trajectories.remove(traj.getLabel());
         }
     }
 
+    /**
+     * String representation of all trajectories in the format the
+     * similarity computation script expects its input to be.
+     *
+     * @return
+     */
     public String toOutputString() {
         String content = "";
         LinkedList<Trajectory> sortedByLabel = new LinkedList<>(trajectories.values());
