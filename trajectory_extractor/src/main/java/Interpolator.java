270e7d1007895fbb4e4a7c0fc3b139438a9088da
simplay <silent.simplay@gmail.com>
2016-03-09 14:46:48 +0100

    Save Array indices access for interpolation + interpolation junit tests

--- trajectory_extractor/src/main/java/Interpolator.java
+++ trajectory_extractor/src/main/java/Interpolator.java
@@ -1,5 +1,6 @@
 public class Interpolator {
 
+
     /**
      * Compute bilinear interpolation of a given flow position
      *
@@ -8,27 +9,42 @@ public class Interpolator {
      * @param y corresponds to column index
      * @return interpolated flow value
      */
-     public double interpolatedValueAt(double[][] data, double x, double y) {
-        int px_i = (int) Math.floor(x);
-        int py_i = (int) Math.floor(y);
+    public double interpolatedValueAt(double[][] data, double x, double y) {
+       int m = data.length;
+       int n = data[0].length;
+       if (x > m-1 || y > n-1) {
+          throw new ArrayIndexOutOfBoundsException("Dimensions (m,n)=("+m+","+n+") but accessing (x,y)=("+x+","+y+")" );
+       }
 
-        int px_i2 = px_i + 1;
-        int py_i2 = py_i + 1;
+       int px_i = (int) Math.floor(x);
+       int py_i = (int) Math.floor(y);
 
-        double dx = x - px_i;
-        double dy = y - py_i;
+       int px_i2 = px_i + 1;
+       int py_i2 = py_i + 1;
 
-        double f_00 = data[px_i][py_i];
-        double f_01 = data[px_i][py_i2];
-        double f_10 = data[px_i2][py_i];
-        double f_11 = data[px_i2][py_i2];
+       double dx = x - px_i;
+       double dy = y - py_i;
 
-        double sum = 0f;
-        sum += f_00*(1.0f-dx)*(1.0-dy);
-        sum += f_01*(1.0f-dx)*dy;
-        sum += f_10*dx*(1.0f-dy);
-        sum += f_11*dx*dy;
+       double f_00 = data[px_i][py_i];
+       double f_01 = saveGetAt(data, px_i, py_i2); // data[px_i][py_i2];
+       double f_10 = saveGetAt(data, px_i2, py_i); // data[px_i2][py_i];
+       double f_11 = saveGetAt(data, px_i2, py_i2); // data[px_i2][py_i2];
 
-        return sum;
+       double sum = 0d;
+       sum += f_00*(1.0d-dx)*(1.0d-dy);
+       sum += f_01*(1.0d-dx)*dy;
+       sum += f_10*dx*(1.0d-dy);
+       sum += f_11*dx*dy;
+
+       return sum;
     }
+
+   protected double saveGetAt(double[][] data, int idx, int idy) {
+      int m = data.length;
+      int n = data[0].length;
+      if (idx == m || idy == n) {
+         return 0d;
+      }
+      return data[idx][idy];
+   }
 }
