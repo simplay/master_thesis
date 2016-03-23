b27e1a6a635c34e0fbfead01d5ffbb047a695061
simplay <silent.simplay@gmail.com>
2016-03-23 21:45:16 +0100

    Removed old similarity code base

--- compute_trajectory_similarities/my_bench.rb
+++
@@ -1,9 +0,0 @@
-require 'benchmark'
-
-n = 5000
-a = (1..n).to_a.map do rand(100) end
-Benchmark.bm do |x|
-  x.report("product") {a.product(a) do |item| t = item.first + item.last end}
-  x.report("combination(2)") {a.combination(2) do |item| t = item.first + item.last end}
-  x.report("each each ") {a.each do |aa| a.each do |bb| t = aa + bb end end}
-end
--- compute_trajectory_similarities/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-echo hint:  use -d for timestep size of 1
-ruby -J-Xmx16000m compute_similarities.rb -l 1 $1 $2 $3 $4 $5 $6 $7 $8
