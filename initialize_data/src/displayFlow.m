db3b893dd97008646720eac2a18dee4241131136
simplay <silent.simplay@gmail.com>
2016-03-24 18:24:55 +0100

    Putting visualizers to common directory

--- initialize_data/src/displayFlow.m
+++
@@ -1,8 +0,0 @@
-function displayFlow(flowfield)
-%DISPLAYFLOW display normalized flow field directions u,v
-    figure('name', 'v flow direction')
-    imshow(normalize(flowfield(:,:,1)));
-    figure('name', 'u flow direction')
-    imshow(normalize(flowfield(:,:,2)));
-end
-
