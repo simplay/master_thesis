f485ce1866546f5665cfd579feb3274704ef321c
simplay <silent.simplay@gmail.com>
2016-04-01 18:41:35 +0200

    Adapted visualizer scripts according to latest pathing conventions

--- Source/visualization/trajectory_visualizer.m
+++ Source/visualization/trajectory_visualizer.m
@@ -1,4 +1,8 @@
-DATASET = 'c14';
+DATASETNAME = 'c14';
+METHODNAME = 'ldof';
+PREFIX = '';
+STEPSIZE_DATA = 8;
+
 img_index = 1;
 START_FRAME = 1;
 END_FRAME = 4;
@@ -7,20 +11,18 @@ SHOULD_PLOT3D = true;
 addpath('../libs/flow-code-matlab');
 addpath('../matlab_shared');
 BASE = '../output/similarities/';
-% 'cars1_step_8_frame_';
-STEPSIZE_DATA = 8;
-PREFIX_FRAME_TENSOR_FILE = [DATASET,'_step_',num2str(STEPSIZE_DATA),'_frame_'];
 
-DATASETNAME = DATASET;
-METHODNAME = 'ldof'; %other,ldof
-DATASETP = strcat(DATASETNAME,'/');
-BASE_FILE_PATH = strcat('../../Data/',METHODNAME,'/',DATASETP);
 
-label_mappings = labelfile2mat(strcat(BASE,DATASET));
-[~, imgs, fwf, bwf] = read_metadata(BASE_FILE_PATH);
+PREFIX_FRAME_TENSOR_FILE = [DATASETNAME, '_step_', num2str(STEPSIZE_DATA), '_frame_'];
+
+DATASET_PATH = strcat(DATASETNAME, '/');
+BASE_FILE_PATH = strcat('../../Data/', DATASET_PATH);
+
+label_mappings = labelfile2mat(strcat(BASE, PREFIX, DATASETNAME));
+[~, imgs, fwf, bwf] = read_metadata(BASE_FILE_PATH, METHODNAME);
 
 fnpath = strcat('../output/tracking_tensor/');
-savefile = strcat(fnpath, strcat(DATASET,'.mat'));
+savefile = strcat(fnpath, strcat(DATASETNAME, '.mat'));
 
 %%
 t = figure;
@@ -36,7 +38,7 @@ num_el = 40;
 close(t);
 
 
-frames = loadAllTrajectoryLabelFrames(DATASET, START_FRAME, END_FRAME);
+frames = loadAllTrajectoryLabelFrames(DATASETNAME, START_FRAME, END_FRAME);
 frame = frames{img_index};
 
 figure('name', 'tracked points');
@@ -52,17 +54,11 @@ end
 len = length(frame.ax);
 row_idx = zeros(len, 1);
 col_idx = zeros(len, 1);
-%for idx=1:len
-    
-%end
+
 row_ids = frame.ax;
 col_ids = frame.ay;
 
-%pixeltensor = tracking_tensor(:,:,:,1);
-%[row_ids, col_ids, ~] = find(pixeltensor(:,:,2) > 0);
-
 distances = sum([row_ids-y,col_ids-x].^2, 2);
-% [~,idx_pos] = min(distances);
 
 % find smallest num_el labels
 [ASorted, AIdx] = sort(distances);
@@ -71,10 +67,6 @@ smallestNIdx = AIdx(1:num_el);
 
 labels = zeros(length(smallestNIdx),1);
 for l_idx = 1:length(smallestNIdx)
-    %smallestNIdx(l_idx)
-    %col_sel = pixeltensor(row_ids(smallestNIdx(l_idx)), col_ids(smallestNIdx(l_idx)), 2);
-    
-    %col_sel = pixeltensor(col_ids(smallestNIdx(l_idx)), row_ids(smallestNIdx(l_idx)), 2);
     labels(l_idx) = frame.labels(smallestNIdx(l_idx));
 end
 
@@ -95,57 +87,50 @@ if SHOULD_PLOT3D
 end
 
 for k=1:2,
-if SHOULD_PLOT3D == false
-    figure('name', strcat('img ', num2str(k)));
-    img = imread(imgs{k});
-    imshow(img)
-    hold on;
-end
-
-tracking_p_range = labels(labels > 0);
-
-drawArrow = @(x,y) quiver( x(1),y(1),x(2)-x(1), y(2)-y(1),0 );
-drawArrow3d = @(x,y,z) quiver3(x(1),y(1),z(1),x(2)-x(1),y(2)-y(1),z(2));
-
-
-for t=1:length(tracking_p_range),
-
-    % find tracked pixels at frame 1
-    %[u,v,w ] = find(tracking_tensor(:,:,1,1) == 1);
-
-    label = tracking_p_range(t);
-    
-    
-    data_idx = smallestNIdx(t);
-    x0 = frame.ax(data_idx);
-    y0 = frame.ay(data_idx);
-    
-    next_frame = frames{img_index+1};
-    
-    
-    next_data_idx = find(next_frame.labels == label);
-    x1 = next_frame.ax(next_data_idx);
-    y1 = next_frame.ay(next_data_idx);
-
-    if isempty(x1) || isempty(y1)
-        continue;
+    if SHOULD_PLOT3D == false
+        figure('name', strcat('img ', num2str(k)));
+        img = imread(imgs{k});
+        imshow(img)
+        hold on;
     end
 
-    %%
-    x = [x0, x1];
-    y = [y0, y1];
-    if SHOULD_PLOT3D
-        z = [0,1];
-        plot3(y0,x0,0, '.r');
-        plot3(y1,x1,1, '.b');
-        drawArrow3d(y,x,z);   
-    else
-        plot(y0,x0, '.r');
-        plot(y1,x1, '.b');
-        drawArrow(y,x);
-    end
-    
-    
+    tracking_p_range = labels(labels > 0);
 
-end
+    drawArrow = @(x,y) quiver( x(1),y(1),x(2)-x(1), y(2)-y(1),0 );
+    drawArrow3d = @(x,y,z) quiver3(x(1),y(1),z(1),x(2)-x(1),y(2)-y(1),z(2));
+
+    for t=1:length(tracking_p_range),
+
+        % find tracked pixels at frame 1
+        label = tracking_p_range(t);
+
+        data_idx = smallestNIdx(t);
+        x0 = frame.ax(data_idx);
+        y0 = frame.ay(data_idx);
+
+        next_frame = frames{img_index+1};
+
+        next_data_idx = find(next_frame.labels == label);
+        x1 = next_frame.ax(next_data_idx);
+        y1 = next_frame.ay(next_data_idx);
+
+        if isempty(x1) || isempty(y1)
+            continue;
+        end
+
+        %%
+        x = [x0, x1];
+        y = [y0, y1];
+        if SHOULD_PLOT3D
+            z = [0,1];
+            plot3(y0,x0,0, '.r');
+            plot3(y1,x1,1, '.b');
+            drawArrow3d(y,x,z);   
+        else
+            plot(y0,x0, '.r');
+            plot(y1,x1, '.b');
+            drawArrow(y,x);
+        end
+        
+    end
 end
