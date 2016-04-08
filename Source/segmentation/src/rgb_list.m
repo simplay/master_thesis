cc94423a133ad9862e5ed751179a19f1553fa47e
simplay <silent.simplay@gmail.com>
2016-04-08 02:03:39 +0200

    Random perm of color and exclude black color

--- Source/segmentation/src/rgb_list.m
+++ Source/segmentation/src/rgb_list.m
@@ -2,20 +2,18 @@ function rgb_values = rgb_list(cluser_count)
 %RGB_LIST generate a list of rgb colors
 %   Detailed explanation goes here
     
-    if (cluser_count < 1 + 2^3)
-        base = 2;
-        rgb_values = zeros(base^3, 3);
-        for k=0:(base^3-1)
-            rgb_values(k+1, :) = asNaryRepresentation(base, k);
+    for base=2:5
+        if (cluser_count < base^3)
+            rgb_values = zeros(base^3, 3);
+            for k=0:(base^3-1)
+                rgb_values(k+1, :) = asNaryRepresentation(base, k);
+            end
+            break;
         end
-    elseif (cluser_count < 1 + 3^3)
-        base = 3;
-        rgb_values = zeros(base^3, 3);
-        for k=0:(base^3 - 1)
-            rgb_values(k+1, :) = asNaryRepresentation(base, k);
-        end
-        
     end
     rgb_values = rgb_values ./(base-1);
+    rgb_values = rgb_values(2:end, :);
+    idxperm = randperm(length(rgb_values)-1);
+    rgb_values = rgb_values(idxperm, :);
 end
 
