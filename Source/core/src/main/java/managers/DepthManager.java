f0690b7cb250f30a33ecd65e03126ffd3fa4359c
simplay <silent.simplay@gmail.com>
2016-04-28 14:59:05 +0200

    Extended docs of depth map warping

--- Source/core/src/main/java/managers/DepthManager.java
+++ Source/core/src/main/java/managers/DepthManager.java
@@ -37,9 +37,14 @@ public class DepthManager {
     /**
      * Warps the existing depth fields in in case the
      * color and depth cameras do not overlap.
+     *
+     * TODO: describe warping mechanism
+     * 
      */
     public static void warpDepthFields() {
         Mat3x4 E = CalibrationManager.extrinsicMat();
+
+        // Get the intrinsic camera calibration
         double p_x = CalibrationManager.depth_principal_point().x();
         double p_y = CalibrationManager.depth_principal_point().y();
         double f_x = CalibrationManager.depth_focal_len().x();
@@ -50,16 +55,22 @@ public class DepthManager {
         double f_x_rgb = CalibrationManager.rgb_focal_len().x();
         double f_y_rgb = CalibrationManager.rgb_focal_len().y();
 
+        // Iterate over all loaded depth maps
         for(int k = 0; k < instance.length(); k++) {
             DepthField df = instance.get(k);
+
+            // initialized the warped depth map in which initially each location
+            // is marked as invalid (i.e. each lookup position is equal to zero).
             DepthField warpedDf = new DepthField(df.m(), df.n());
 
             for (int i = 0; i < df.m(); i++) {
                 for (int j = 0; j < df.n(); j++) {
                     double d = df.valueAt(i, j);
                     Point3d p = new Point3d((i-p_x)/f_x, (j-p_y)/f_y, 1d);
-                    p.scaleBy(d).applyTransformation(E);
+                    p.scaleBy(d).transformBy(E);
                     double d_tilde = p.z();
+
+                    // Compute lookup coordinates in warped depth map
                     int i_tilde = (int)((p.x()*f_x_rgb) / d_tilde + p_x_rgb);
                     int j_tilde = (int)((p.y()*f_y_rgb) / d_tilde + p_y_rgb);
 
