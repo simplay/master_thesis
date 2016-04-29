14b583bbf1f15c01903aa15e9400cd43a3cb5dbf
simplay <silent.simplay@gmail.com>
2016-04-29 13:21:42 +0200

    Updated method signature of main_min_cut script

--- Source/segmentation/main_min_cut.m
+++ Source/segmentation/main_min_cut.m
@@ -7,13 +7,16 @@ addpath('../matlab_shared');
 addpath('../libs/flow-code-matlab');
 addpath('../libs/GCMex/');
 
-DATASET = 'alley2small';
+DATASET = 'cars';
 METHODNAME = 'ldof';
-PREFIX_OUTPUT_FILENAME = 'mc_d';
-PREFIX_INPUT_FILENAME = 'md_nn_1000_best';
+PREFIX_OUTPUT_FILENAME = 'md_d';
+PREFIX_INPUT_FILENAME = 'foobar_md_nn_1000_best';
 
 % should the eigendecomposition be computed
 COMPUTE_EIGS = false;
+
+% Should the label assignment be re-used
+REUSE_LABEL_ASSIGNMENT = false;
 
 % should the numerical fast eigs method be used
 USE_EIGS = true;
@@ -24,7 +27,7 @@ COMPUTE_FULL_RANGE = false;
 
 % use a prespecified number of eigenvectors
 USE_CLUSER_EW_COUNT = true;
-FORCE_EW_COUNT = 12;
+FORCE_EW_COUNT = 7;
 
 THRESH = 0.0000;
 
@@ -35,7 +38,7 @@ NU = 0.000000001;
 
 
 % number of clusters we want to segment the given sequence
-CLUSTER_CENTER_COUNT = 8;
+CLUSTER_CENTER_COUNT = 3;
 
 %
 % RUN_MODE = 1 => vis segmentation
@@ -57,10 +60,10 @@ FILTER_ZERO_EIGENVALUES = false;
 SELECT_AFFINITY_IDX = false;
 SELECTED_ENTITY_IDX = 64;
 SELECTED_ENTITY_IDX = 1;
-frame_idx = 40;
+frame_idx = 3;
 
 % number of iterations that should be performed for computing the approx.
-num_of_iters = 3;
+num_of_iters = 5;
 
 PREFIX_OUTPUT_FILENAME = strcat(PREFIX_OUTPUT_FILENAME, '_iters_', num2str(num_of_iters), '_c_', num2str(CLUSTER_CENTER_COUNT));
 if USE_CLUSER_EW_COUNT
@@ -70,6 +73,6 @@ PREFIX_OUTPUT_FILENAME = strcat(PREFIX_OUTPUT_FILENAME, '_nu_', num2str(NU));
 %%
 if exist('W','var') == 0
     disp('setting initial values...')
-    W = 1; U_full = 1; S_full = 1;
+    W = 1; U_full = 1; S_full = 1; label_assignments = 1;
 end
-[W, U, S, U_full, S_full] = run_min_cut(DATASET, METHODNAME, RUN_MODE, CLUSTER_CENTER_COUNT, THRESH, COMPUTE_EIGS, USE_EIGS, W, SELECTED_ENTITY_IDX, frame_idx, USE_CLUSER_EW_COUNT, num_of_iters, FORCE_EW_COUNT, U_full, S_full, COMPUTE_FULL_RANGE, SAVE_FIGURES, SHOW_SEGMENTATION, PREFIX_OUTPUT_FILENAME, PREFIX_INPUT_FILENAME, NU, FILTER_ZERO_EIGENVALUES);
+[W, U, S, U_full, S_full, label_assignments] = run_min_cut(DATASET, METHODNAME, RUN_MODE, CLUSTER_CENTER_COUNT, THRESH, COMPUTE_EIGS, USE_EIGS, W, SELECTED_ENTITY_IDX, frame_idx, USE_CLUSER_EW_COUNT, num_of_iters, FORCE_EW_COUNT, U_full, S_full, COMPUTE_FULL_RANGE, SAVE_FIGURES, SHOW_SEGMENTATION, PREFIX_OUTPUT_FILENAME, PREFIX_INPUT_FILENAME, NU, FILTER_ZERO_EIGENVALUES, REUSE_LABEL_ASSIGNMENT, label_assignments);
