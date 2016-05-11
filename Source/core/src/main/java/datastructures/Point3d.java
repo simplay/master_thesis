7c8c8081a6473f4a44c3dd187c161f584f75696b
simplay <silent.simplay@gmail.com>
2016-05-11 23:41:28 +0200

    Completed docs of Point3d

--- Source/core/src/main/java/datastructures/Point3d.java
+++ Source/core/src/main/java/datastructures/Point3d.java
@@ -1,8 +1,12 @@
 package datastructures;
 
-import managers.DepthManager;
-
+/**
+ * Models a 3d dimensional point in the Euclidean space.
+ * Supports basic vector operations.
+ */
 public class Point3d {
+
+    // The components of this 3d point
     private double x;
     private double y;
     private double z;
@@ -10,16 +14,36 @@ public class Point3d {
     // A Point is invalid if it has a depth value equal to 0.0d
     private boolean isValid = true;
 
+    /**
+     * Construct a new Point by providing the 3 component values.
+     *
+     * @param x 1st component
+     * @param y 2nd component
+     * @param z 3rd component
+     */
     public Point3d(double x, double y, double z) {
         this.x = x;
         this.y = y;
         this.z = z;
     }
 
+    /**
+     * Make a shallow copy of this Point3d instance.
+     *
+     * @return shallow copy
+     */
     public Point3d copy() {
-        return new Point3d(x,y,z);
+        return new Point3d(x, y, z);
     }
 
+    /**
+     * Subtract another 3d from this point.
+     *
+     * Note that the component of this points are modified when calling this method.
+     *
+     * @param other reference point we subtract from this point.
+     * @return updated version of this point
+     */
     public Point3d sub(Point3d other) {
         this.x -= other.x();
         this.y -= other.y();
@@ -28,18 +52,36 @@ public class Point3d {
         return this;
     }
 
+    /**
+     * Checks whether this point is marked as invalid.
+     *
+     * This is-valid-flag is used to filter invalid 3d points (e.g. points,
+     * that do not exhibit a valid depth value).
+     * 
+     * @return true if this point is invalid, false otherwise (by default).
+     */
     public boolean isValid() {
         return isValid;
     }
 
+    /**
+     * Marks the state of this point as invalid.
+     * This state is not copied when crating a copy of this instance.
+     */
     public void markInvalid() {
         this.isValid = false;
     }
 
     /**
-     * Scales this point3 by an extrinsic camera 3x4 double matrix.
+     * Apply a (3 x 4) matrix to this 3d point
+     * Herby, we interpret the 3d point as a 4d homogeneous point.
      *
-     * @param mat 3x4 matrix
+     * Thus, a scale and shift is applied to this 3d point.
+     *
+     * The matrix we usually apply to a 3d point is the so called
+     * extrinsic camera matrix.
+     *
+     * @param mat 3x4 (extrinsic) matrix
      */
     public Point3d transformBy(Mat3x4 mat) {
         Point3d col1 = mat.getCol(0);
@@ -54,6 +96,13 @@ public class Point3d {
         return this;
     }
 
+    /**
+     * Divides this point by a given scale.
+     * This modifies the components of this point.
+     *
+     * @param f the divisor we use to divide the components of this point.
+     * @return this point after being modified.
+     */
     public Point3d div_by(double f) {
         x /= f;
         y /= f;
@@ -61,6 +110,13 @@ public class Point3d {
         return this;
     }
 
+    /**
+     * Scales the components of this point by a given scale.
+     * This modifies the components of this point.
+     *
+     * @param f the factor we use to divide the components of this point..
+     * @return this point after being modified.
+     */
     public Point3d scaleBy(double f) {
         x *= f;
         y *= f;
@@ -68,30 +124,56 @@ public class Point3d {
         return this;
     }
 
+    /**
+     * Computes the squared l2 distance of this point.
+     *
+     * @return squared l2 distance.
+     */
     public double length_squared() {
         return x*x + y*y + z*z;
     }
 
     /**
-     * Computed the l2 distance to the origin
-     * @return
+     * Computed the l2 distance to the origin.
+     *
+     * @return l2 distance
      */
     public double length() {
         return Math.sqrt(x*x + y*y + z*z);
     }
 
+    /**
+     * Get the x component of this 3d point
+     *
+     * @return x component
+     */
     public double x() {
         return x;
     }
 
+    /**
+     * Get the y component of this 3d point
+     *
+     * @return y component
+     */
     public double y() {
         return y;
     }
 
+    /**
+     * Get the z component of this 3d point
+     *
+     * @return z component
+     */
     public double z() {
         return z;
     }
 
+    /**
+     * Get the pretty string representation used for dubbing.
+     *
+     * @return debuggable representation of this 3d point.
+     */
     public String toString() {
         return "(x,y,z)=("+ x + "," + y + "," + z + ")";
     }
