2ff0eb535ee61c904624210d2901e9df1044c742
simplay <silent.simplay@gmail.com>
2016-05-17 18:02:44 +0200

    Testing CalibrationsReader class

--- Source/core/src/main/java/readers/CalibrationsReader.java
+++ Source/core/src/main/java/readers/CalibrationsReader.java
@@ -8,12 +8,25 @@ import similarity.DataChecker;
 
 import java.util.ArrayList;
 
+/**
+ * CalibrationsReader reads the content of a `calib.txt` file.
+ * Such files are located at `./Data/DATASET/meta/`.
+ *
+ * The valid format is described in the README.md located at `./Data/`
+ */
 public class CalibrationsReader extends FileReader {
 
+    // The read File lines modeled as labeled file lines.
     public ArrayList<LabeledFileLine> fLines;
 
-    public CalibrationsReader(String dataset) {
-        String baseFileName = "../../Data/" + dataset + "/meta/calib.txt";
+    /**
+     * General constructor
+     *
+     * @param dataset dataset we are running.
+     * @param basePath base file path where files are located.
+     */
+    public CalibrationsReader(String dataset, String basePath) {
+        String baseFileName = basePath + dataset + "/meta/calib.txt";
         if (DataChecker.hasCalibrationData(baseFileName)) {
             fLines = new ArrayList<>();
             readFile(baseFileName);
@@ -23,6 +36,21 @@ public class CalibrationsReader extends FileReader {
         }
     }
 
+    /**
+     * Constructor used by the pipeline.
+     * Convention: Files are located at "../../Data/"
+     *
+     * @param dataset dataset we are running.
+     */
+    public CalibrationsReader(String dataset) {
+        this(dataset, "../../Data/");
+    }
+
+    /**
+     * Parse a file line according to the specified format of a calib.txt file.
+     *
+     * @param line a line of the calib.txt file
+     */
     @Override
     protected void processLine(String line) {
         if (!line.isEmpty()) {
