ac6100c55996a4cbeb4c300d08b88f494525270c
simplay <silent.simplay@gmail.com>
2016-05-18 16:30:38 +0200

    Fully tested CalibrationManager class

--- Source/core/src/test/java/CalibrationsReaderTest.java
+++ Source/core/src/test/java/CalibrationsReaderTest.java
@@ -28,37 +28,7 @@ public class CalibrationsReaderTest {
         assertEquals(249.031, cm.getRgbPrincipalPoint().x(), 0);
         assertEquals(346.471, cm.getRgbPrincipalPoint().y(), 0);
 
-        assertEquals(503.905, CalibrationManager.depth_focal_len().x(), 0);
-        assertEquals(504.261, CalibrationManager.depth_focal_len().y(), 0);
-
-        assertEquals(272.202, CalibrationManager.depth_principal_point().x(), 0);
-        assertEquals(352.457, CalibrationManager.depth_principal_point().y(), 0);
-
-        assertEquals(574.394, CalibrationManager.rgb_focal_len().x(), 0);
-        assertEquals(573.71, CalibrationManager.rgb_focal_len().y(), 0);
-
-        assertEquals(249.031, CalibrationManager.rgb_principal_point().x(), 0);
-        assertEquals(346.471, CalibrationManager.rgb_principal_point().y(), 0);
-
         Mat3x4 E = cm.getExtrinsicMat();
-
-        assertEquals(0.999749, E.getCol(0).x(), 0);
-        assertEquals(-0.0051649, E.getCol(0).y(), 0);
-        assertEquals(-0.0218031, E.getCol(0).z(), 0);
-
-        assertEquals(0.00518867 , E.getCol(1).x(), 0);
-        assertEquals(0.999986 , E.getCol(1).y(), 0);
-        assertEquals(0.00103363, E.getCol(1).z(), 0);
-
-        assertEquals(0.0217975 , E.getCol(2).x(), 0);
-        assertEquals(-0.0011465 , E.getCol(2).y(), 0);
-        assertEquals(0.999762, E.getCol(2).z(), 0);
-
-        assertEquals(0.0243073, E.getCol(3).x(), 0);
-        assertEquals(-0.000166518, E.getCol(3).y(), 0);
-        assertEquals(0.0151706, E.getCol(3).z(), 0);
-
-        E = CalibrationManager.extrinsicMat();
 
         assertEquals(0.999749, E.getCol(0).x(), 0);
         assertEquals(-0.0051649, E.getCol(0).y(), 0);
