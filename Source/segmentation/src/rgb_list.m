00bf712e71e23f951bd4bead2bc3b3dc0bbf1bf8
simplay <silent.simplay@gmail.com>
2016-05-19 14:21:51 +0200

    Bugfix: crash when cluster count equal color count
    since we remove the color 'white' there is actually one color less,
    which results in a runtime error, when using exactly that count.
    Workaround: Strengthened boundary condition check.

--- Source/segmentation/src/rgb_list.m
+++ Source/segmentation/src/rgb_list.m
@@ -2,8 +2,8 @@ function rgb_values = rgb_list(cluser_count)
 %RGB_LIST generate a list of rgb colors
 %   Detailed explanation goes here
     
-    for base=3:5
-        if (cluser_count < base^3)
+    for base=3:6
+        if (cluser_count < base^3 - 1)
             rgb_values = zeros(base^3, 3);
             for k=0:(base^3-1)
                 rgb_values(k+1, :) = asNaryRepresentation(base, k);
