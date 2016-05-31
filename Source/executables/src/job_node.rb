30a150593546bc99a8495b18d03329dc0c1ea6df
simplay <silent.simplay@gmail.com>
2016-05-31 19:09:43 +0200

    Support option values for job nodes

--- Source/executables/src/job_node.rb
+++ Source/executables/src/job_node.rb
@@ -2,7 +2,14 @@ class JobNode
   def initialize(klass, query, collection = nil, use_index_value = false)
     @klass = klass
     @query = query
-    @collection = collection
+    @uses_hash = false
+    if collection.is_a? Hash
+      @uses_hash = true
+      @collection = collection.values
+      @keys = collection.keys
+    else
+      @collection = collection
+    end
     @use_index_value = use_index_value
   end
 
@@ -17,10 +24,14 @@ class JobNode
     selection = gets.chomp
     unless @collection.nil?
       sel_idx = selection.to_i
-      selection = @collection[sel_idx]
+      selection = options[sel_idx]
       selection = (sel_idx + 1) if use_index?
     end
     @klass.new(selection)
+  end
+
+  def options
+    (@uses_hash) ? @keys : @collection
   end
 
   def use_index?
