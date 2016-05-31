9dfb822c187419c9364bfb5af77d2f42132d73a4
Michael Single <silent.simplay@gmail.com>
2016-06-01 00:18:53 +0200

    Update run.bat

--- Source/executables/run.bat
+++ Source/executables/run.bat
@@ -1,2 +1,28 @@
 @echo off
+
+:START
+echo "Please enter the number of one the options below:"
+echo "+ [-1] Exit program"
+echo "+ [1]  Run core"
+echo "+ [2]  run core"
+echo "+ [3]  run core"
+echo "+ [4]  run core"
+set /p input= "Selecting "
+
+if %input% == 1 (
+echo "running core"
+goto RUN_CORE
+) else (
+if %input% == -1 (
+echo "Exiting program..."
+goto END
+)
+)
+
+
+:RUN_CORE
+echo "foobar"
+goto START
+
+:END
 pause
