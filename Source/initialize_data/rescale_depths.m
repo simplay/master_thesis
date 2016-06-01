e392fc4e19d055033deda3fd14dfc649e87f303d
simplay <silent.simplay@gmail.com>
2016-06-01 18:18:27 +0200

    Implemented helper function to rescale Bonn dataset depths

---
+++ Source/initialize_data/rescale_depths.m
@@ -0,0 +1,33 @@
+function rescale_depths( input_filepath, img_ext, scale, output_path, match )
+% rescale_bonn_depths allows to rescale a set of depth fields and save the
+% rescaled images in a new target directory. Used to rescale Bonn depth
+% fields.
+% example usage:
+% in = '~/repos/rgbd_layered_flow_code/data/our_data/chair/1_4/'
+% out = '~/repos/rgbd_layered_flow_code/data/our_data/chair/1_4/rescaled/'
+% match = 'depth' % only select files including this substring
+% ext = '.png' % fileextension of files we'd like to rescale
+% scale = 5; % divides image channels by this scale, bonn data 5 => mm
+% scales.
+% rescale_depths(in, ext, 5, out, match);
+    % filepath #=> '~/foobar/'
+    % img_ext #=> '.png'
+    % scale #=> 3
+    % output_path #=> '~/foobar/rescaled/'
+    
+    % create target directory in case it does not exist yet
+    if ~exist('name','dir')
+        mkdir(output_path);
+    end
+    
+    files_to_select = strcat(input_filepath, match,'*', img_ext);
+    src_files = dir(files_to_select);
+    for i = 1 : length(src_files)
+        filename = src_files(i).name;
+        input_file_path_name = strcat(input_filepath, filename);
+        img = imread(input_file_path_name);
+        output_file_path_name = strcat(output_path, filename);
+        imwrite(img/scale, output_file_path_name);
+    end
+end
+
