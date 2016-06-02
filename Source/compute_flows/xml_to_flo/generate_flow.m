5ed2acda8cd0a63ca3b9f785ff3b479cb9a12d21
simplay <silent.simplay@gmail.com>
2016-06-02 15:52:07 +0200

    Updated functionality when computing the optical flow using the SRSF method
    + Updated the srsf executable to use its latest version (see
    github.com/simplay/modified_srsf_method/)
    + Adapted and improved  xml to .flo file extraction matlab script

--- Source/compute_flows/xml_to_flo/generate_flow.m
+++ Source/compute_flows/xml_to_flo/generate_flow.m
@@ -4,34 +4,45 @@ function generate_flow(in_path, fname, out_path)
     
     identifier = strsplit(fname, '.xml');
     xml_flow_file = xml_read(strcat(in_path,fname));
-
-    flow_x = xml_flow_file.SFx;
-    flow_data = flow_x.data;
-    modifiedStr = strrep(flow_data, '0.', '0');
-    parsed_flowX = strsplit(modifiedStr);
-   % flowX = reshape(str2num(char(parsed_flow)), flow_x.cols, flow_x.rows)';
     
+    M = xml_flow_file.Mask.rows;
+    N = xml_flow_file.Mask.cols;
     
-    flow_y = xml_flow_file.SFy;
-    flow_data = flow_y.data;
-    modifiedStr = strrep(flow_data, '0.', '0');
-    parsed_flowY = strsplit(modifiedStr);
-    %flowY = reshape(str2num(char(parsed_flow)), flow_y.cols, flow_y.rows)';
+    parsed_flowX = parsed_xml_member(xml_flow_file.SFx);
+    parsed_flowY = parsed_xml_member(xml_flow_file.SFx);
+    parsed_flowZ = parsed_xml_member(xml_flow_file.SFx);
+    parsed_flow_ofx = parsed_xml_member(xml_flow_file.OFx);
+    parsed_flow_ofy = parsed_xml_member(xml_flow_file.OFy);
+    
+    t = xml_flow_file.Translation.data;
+    q = xml_flow_file.Rotation.data;
     
     % write flow directions
-    flowY = zeros(flow_y.cols, flow_y.rows);
-    flowX = zeros(flow_x.cols, flow_x.rows);
+    flowY = zeros(M, N);
+    flowX = zeros(M, N);
+    flowZ = zeros(M, N);
+    OFx = zeros(M, N);
+    OFy = zeros(M, N);
+    
     idx = 1;
-    for k=1:flow_y.rows
-        for l=1:flow_y.cols
+    for k=1:M
+        for l=1:N
+            OFx(k, l) = str2double(parsed_flow_ofx{idx});
+            OFy(k, l) = str2double(parsed_flow_ofy{idx});
             flowX(k, l) = str2double(parsed_flowX{idx});
             flowY(k, l) = str2double(parsed_flowY{idx});
+            flowZ(k, l) = str2double(parsed_flowZ{idx});
             idx = idx + 1;
         end
     end
 
-    img = mat2img(flowX, flowY);
+    img = mat2img(OFx, OFy);
     out_fname = strcat(out_path, char(identifier(1)), '.flo');
     writeFlowFile(img(:,:,1:2), out_fname);
+end
+
+function parse = parsed_xml_member(member)
+    modifiedStr = strrep(member.data, '0.', '0');
+    parse = strsplit(modifiedStr);
 end
 
