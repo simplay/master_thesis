2dbbe21c36f73027a160fc948966a436caaeb221
simplay <silent.simplay@gmail.com>
2016-07-07 10:55:32 +0200

    Allow to visualize several trajectory tracking regions

--- Source/visualization/trajectory_visualizer.m
+++ Source/visualization/trajectory_visualizer.m
@@ -12,7 +12,8 @@ SHOULD_PLOT3D = true;
 img_index = 1;
 START_FRAME = 1;
 END_FRAME = 4;
-num_el = 10;
+num_el = 5;
+selection_count = 2;
 
 % load relevant trajectory associated data
 [imgs, fwf, bwf, boundaries, label_mappings, frames] = load_trajectory_data(DATASET, METHODNAME, PREFIX_INPUT_FILENAME, START_FRAME, END_FRAME);
@@ -25,7 +26,7 @@ I = mat2img(img(:,:,1));
 imshow(I);
 
 % plot all tracked points
-[x, y] = ginput(1);
+[xx, yy] = ginput(selection_count);
 close(t);
 
 frame = frames{img_index};
@@ -46,79 +47,86 @@ col_idx = zeros(len, 1);
 row_ids = frame.ax;
 col_ids = frame.ay;
 
-distances = sum([row_ids-y,col_ids-x].^2, 2);
+% TODO iterate over for loop here
+for k=1:length(xx)
+    x = xx(k);
+    y = yy(k);
+    distances = sum([row_ids-y,col_ids-x].^2, 2);
 
-% find smallest num_el labels
-[ASorted, AIdx] = sort(distances);
-smallestNElements = ASorted(1:num_el);
-smallestNIdx = AIdx(1:num_el);
+    % find smallest num_el labels
+    [ASorted, AIdx] = sort(distances);
+    smallestNElements = ASorted(1:num_el);
+    smallestNIdx = AIdx(1:num_el);
 
-labels = zeros(length(smallestNIdx),1);
-for l_idx = 1:length(smallestNIdx)
-    labels(l_idx) = frame.labels(smallestNIdx(l_idx));
-end
-
-
-%%
-disp('the FROM position in img1 has to correspond to the TO position in img2.');
-
-if SHOULD_PLOT3D
-    img1 = imread(imgs{1});
-    img1 = img1(:,:,1);
-    img2 = imread(imgs{2});
-    img2 = img2(:,:,1);
-    figure
-    surf(0*ones(size(img1)), img1,'EdgeColor','none')
-    hold on
-    surf(1*ones(size(img2)), img2,'EdgeColor','none')
-    
-end
-
-for k=1:2,
-    if SHOULD_PLOT3D == false
-        figure('name', strcat('img ', num2str(k)));
-        img = imread(imgs{k});
-        imshow(img)
-        hold on;
+    labels = zeros(length(smallestNIdx),1);
+    for l_idx = 1:length(smallestNIdx)
+        labels(l_idx) = frame.labels(smallestNIdx(l_idx));
     end
 
-    tracking_p_range = labels(labels > 0);
 
-    drawArrow = @(x,y) quiver( x(1),y(1),x(2)-x(1), y(2)-y(1),0 );
-    drawArrow3d = @(x,y,z) quiver3(x(1),y(1),z(1),x(2)-x(1),y(2)-y(1),z(2));
+    %%
+    disp('the FROM position in img1 has to correspond to the TO position in img2.');
 
-    for t=1:length(tracking_p_range),
+    if SHOULD_PLOT3D
+        img1 = imread(imgs{1});
+        img1 = img1(:,:,1);
+        img2 = imread(imgs{2});
+        img2 = img2(:,:,1);
+        if k == 1
+            figure
+        end
+        surf(0*ones(size(img1)), img1,'EdgeColor','none')
+        hold on
+        surf(1*ones(size(img2)), img2,'EdgeColor','none')
 
-        % find tracked pixels at frame 1
-        label = tracking_p_range(t);
+    end
 
-        data_idx = smallestNIdx(t);
-        x0 = frame.ax(data_idx);
-        y0 = frame.ay(data_idx);
-
-        next_frame = frames{img_index+1};
-
-        next_data_idx = find(next_frame.labels == label);
-        x1 = next_frame.ax(next_data_idx);
-        y1 = next_frame.ay(next_data_idx);
-
-        if isempty(x1) || isempty(y1)
-            continue;
+    for k=1:2,
+        if SHOULD_PLOT3D == false
+            figure('name', strcat('img ', num2str(k)));
+            img = imread(imgs{k});
+            imshow(img)
+            hold on;
         end
 
-        %%
-        x = [x0, x1];
-        y = [y0, y1];
-        if SHOULD_PLOT3D
-            z = [0,1];
-            plot3(y0,x0,0, '.r');
-            plot3(y1,x1,1, '.b');
-            drawArrow3d(y,x,z);   
-        else
-            plot(y0,x0, '.r');
-            plot(y1,x1, '.b');
-            drawArrow(y,x);
+        tracking_p_range = labels(labels > 0);
+
+        drawArrow = @(x,y) quiver( x(1),y(1),x(2)-x(1), y(2)-y(1),0 );
+        drawArrow3d = @(x,y,z) quiver3(x(1),y(1),z(1),x(2)-x(1),y(2)-y(1),z(2));
+
+        for t=1:length(tracking_p_range),
+
+            % find tracked pixels at frame 1
+            label = tracking_p_range(t);
+
+            data_idx = smallestNIdx(t);
+            x0 = frame.ax(data_idx);
+            y0 = frame.ay(data_idx);
+
+            next_frame = frames{img_index+1};
+
+            next_data_idx = find(next_frame.labels == label);
+            x1 = next_frame.ax(next_data_idx);
+            y1 = next_frame.ay(next_data_idx);
+
+            if isempty(x1) || isempty(y1)
+                continue;
+            end
+
+            %%
+            x = [x0, x1];
+            y = [y0, y1];
+            if SHOULD_PLOT3D
+                z = [0,1];
+                plot3(y0,x0,0, '.r');
+                plot3(y1,x1,1, '.b');
+                drawArrow3d(y,x,z);   
+            else
+                plot(y0,x0, '.r');
+                plot(y1,x1, '.b');
+                drawArrow(y,x);
+            end
+
         end
-        
     end
 end
